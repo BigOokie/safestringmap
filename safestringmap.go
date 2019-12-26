2e21db68cebead3c75d07588197f24a09efc7b5d
BigOokie <36015380+BigOokie@users.noreply.github.com>
2019-12-27 09:06:42 +1100

    Update comments

--- safestringmap.go
+++ safestringmap.go
@@ -10,25 +10,23 @@ package safestringmap
 
 import "sync"
 
-// SafeStringMap defines a map structure of strings as well as a RWLock to
+// SafeStringMap structure. Defines a map of strings that are managed by a `sync.RWLock` to
 // ensure safe Read-Write access in concurrent environments.
-// All read operations utilse the RLock.
-// All write operations utilise the Lock.
+// All read operations utilse `RLock`. All write operations utilise `Lock`.
 type SafeStringMap struct {
 	sync.RWMutex
 	internal map[string]string
 }
 
-// NewSafeStringMap creates a new instance of a SafeStringMap
+// NewSafeStringMap function creates a new instance of a `SafeStringMap` structure.
 func NewSafeStringMap() *SafeStringMap {
 	return &SafeStringMap{
 		internal: make(map[string]string),
 	}
 }
 
-// Load will return the string value associated with the requested
-// string key if it exists in the map. If not an emptry string will be
-// returned and the ok bool will be set to false.
+// Load returns the string value associated with the requested key if it exists in the map.
+// If not an emptry string is returned and the functions `bool` result will be set to `false`.
 func (ssm *SafeStringMap) Load(key string) (value string, ok bool) {
 	ssm.RLock()
 	result, ok := ssm.internal[key]
@@ -36,24 +34,22 @@ func (ssm *SafeStringMap) Load(key string) (value string, ok bool) {
 	return result, ok
 }
 
-// Delete will removed a value from the map that is associated with the
-// specified key value - if it exists.
+// Delete removes a value from the map that is associated with the specified key value - if it exists.
 func (ssm *SafeStringMap) Delete(key string) {
 	ssm.Lock()
 	delete(ssm.internal, key)
 	ssm.Unlock()
 }
 
-// Store will add a new key value pair to the map if it doesnt already exist.
-// If the key already exists, the provided string value will be used to update
-// the value contained within the map with the new provided value
+// Store adds a new key value pair to the map if it doesnt already exist.
+// If the key already exists, the provided string value will be used to update (overwrite) the value associated with the key.
 func (ssm *SafeStringMap) Store(key, value string) {
 	ssm.Lock()
 	ssm.internal[key] = value
 	ssm.Unlock()
 }
 
-// Count will return the count of the number of objects stored within the map as an integer
+// Count returns the count of strings stored within the map (as an integer)
 func (ssm *SafeStringMap) Count() int {
 	ssm.RLock()
 	result := len(ssm.internal)
